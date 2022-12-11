3a2d2d92f1d7377eee950ff386dabe71b3576ab0
Ronald <rvpostha15@yahoo.com>
2022-12-11 18:57:18 -0500

    commit tree d89054c1fda1d5a0398c16a76838c89265f88752

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (feet <= 2500) {
+  return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.':'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip) {
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.';
+      break;
+  }
+  return charm;
 }
